5e38a71c63fd442f9b016ca466d34bb965a49c17
YinLei <37959310+yinleiCoder@users.noreply.github.com>
2020-07-12 12:30:37 +0800

    app init

---
+++ app/src/main/java/com/yinlei/appupdate/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.yinlei.appupdate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * 应用内升级APP
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
